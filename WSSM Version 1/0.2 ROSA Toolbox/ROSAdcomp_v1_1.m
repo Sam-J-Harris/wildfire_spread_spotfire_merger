896bb0691fda40b25c27996deada178acb5c3f88
Sam Harris <sam.harris.16@ucl.ac.uk>
2024-08-13 12:28:54 +0100

    Update ROSAdcomp_v1_1.m
    add comments to ROSAdcomp

--- WSSM Version 1/0.2 ROSA Toolbox/ROSAdcomp_v1_1.m
+++ WSSM Version 1/0.2 ROSA Toolbox/ROSAdcomp_v1_1.m
@@ -1,22 +1,18 @@
 function bigDataPack = ROSAdcomp_v1_1(bigZ1, bigC1, bigJ1,merdata1,tmax1,prdt,shswt)
-if shswt==2 || shswt==21
+% ROSA data compilation function
+%   = compiles previous wildfire data (if applicable) and current wildfire
+%   data into one bigDataPack for easy saving and later use
+%
+%   Contains bigZ, bigC, bigJ, merdata and tmax data.
+%
+% Code:
+if shswt==2 || shswt==21 % adding previous data to the data pack (if applicable).
     load(prdt,'bigDataPack');
     bigZ0 = bigDataPack{1}; bigC0 = bigDataPack{2}; bigJ0 = bigDataPack{3}; merdata0 = bigDataPack{4}; tmax0 = bigDataPack{5};
-    bigZ = [bigZ0 bigZ1(:,2:end)]; bigC = [bigC0 bigC1(:,2:end)]; bigJ = [bigJ0 bigJ1(2:end)];
+    bigZ = [bigZ0 bigZ1(:,2:end)]; bigC = [bigC0 bigC1(:,2:end)]; bigJ = [bigJ0 bigJ1(2:end)]; % joins previous and current data together.
     merdata = [merdata0, merdata1]; tmax = tmax0+tmax1;
     bigDataPack = {bigZ,bigC,bigJ,merdata,tmax};
 else
     bigDataPack = {bigZ1,bigC1,bigJ1,merdata1,tmax1}; % big data pack for easy saving
 end
 end
-
-% % Test
-% prdt1 = 'bigDataPack_v1_2.mat'; prdt2 = 'bigDataPack_v1_2_2.mat';
-% load(prdt1,'bigDataPack'); bigDataPacka = bigDataPack;
-% load(prdt2,'bigDataPack'); bigDataPackb = bigDataPack;
-% 
-% bigZ0 = bigDataPacka{1}; bigC0 = bigDataPacka{2}; bigJ0 = bigDataPacka{3}; merdata0 = bigDataPacka{4};
-% bigZ1 = bigDataPackb{1}; bigC1 = bigDataPackb{2}; bigJ1 = bigDataPackb{3}; merdata1 = bigDataPackb{4};
-% bigZ = [bigZ0 bigZ1(:,2:end)]; bigC = [bigC0 bigC1(:,2:end)]; bigJ = [bigJ0 bigJ1(2:end)];
-% merdata = [merdata0, merdata1];
-% bigDataPack = {bigZ,bigC,bigJ,merdata};
