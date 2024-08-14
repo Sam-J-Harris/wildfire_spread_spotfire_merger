b6aa25b358f4f391b2fb71934ede536f2ca37f43
Sam Harris <sam.harris.16@ucl.ac.uk>
2024-08-14 16:15:16 +0100

    renaming folders
    renamed the ROSS and ROSA toolboxes. temporarily removed Hilton compare folder to work on offline. Will be reuploaded once edits complete.

--- WSSM Version 1/1 Figs10-12 Hilton compare/0.2 M Toolbox v1/ROSAzpresent_CR_v1_2.m
+++
@@ -1,9 +0,0 @@
-% Presenting Solution
-clc; close all; clear; set(0,'DefaultFigureVisible','on'); warning('off');
-
-prevdata = 'v1_2_t11_alpha0.0_t0-600_workspace.mat'; % bring in data
-%prevdata = 'v1_2_v04_t10.5_alpha0.5_t0-800_workspace.mat'; % bring in data
-load(prevdata,'bigDataPack');
-bigZ1 = bigDataPack{1}; bigC1 = bigDataPack{2}; bigJ1 = bigDataPack{3};
-spc = 100; shswit = 2; %spc=20;
-ROSAplot_CR_v1_2(bigZ1,bigJ1,spc,shswit,1);
